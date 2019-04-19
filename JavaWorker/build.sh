f2bd0b4a0737ea48c8df1d89382c353e7b840740
Benjamin Singer <benjaminjsinger@gmail.com>
2019-04-18 21:09:40 -0500

    get generated code for java

--- JavaWorker/build.sh
+++ JavaWorker/build.sh
@@ -8,6 +8,23 @@ echo "========================="
 # Download the dependenties in case they are not present
 ../SpatialOS/scripts/download_dependencies.sh
 
+OUT_DIR="$(pwd)/src/generated/java/"
+rm -rf "${OUT_DIR}"
+mkdir -p "${OUT_DIR}"
+
+
+# Why the f arent these set anymore?
+SDK_VERSION="13.5.1"
+TOOLS_DIR="$(pwd)/../SpatialOS/tools/${SDK_VERSION}"
+SCHEMA_DIR="$(pwd)/../SpatialOS/schema"
+
+"${TOOLS_DIR}"/schema_compiler/schema_compiler \
+  --schema_path="${SCHEMA_DIR}" \
+  --java_out="${OUT_DIR}" \
+  --load_all_schema_on_schema_path \
+  "${SCHEMA_DIR}"/*.schema \
+  "${SCHEMA_DIR}"/improbable/*.schema
+
 ./gradlew buildJar
 if [ $? -ne 0 ]; then
     echo "========================="
