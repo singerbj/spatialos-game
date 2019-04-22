0da2c9339c56d77d4221f268db8c49a794da82f3
Benjamin Singer <benjaminjsinger@gmail.com>
2019-04-22 17:22:45 -0500

    javaworker should work, remove snapshot

--- JavaWorker/src/main/java/com/javaworker/JavaWorker.java
+++ JavaWorker/src/main/java/com/javaworker/JavaWorker.java
@@ -1,16 +1,21 @@
 package com.javaworker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import demo.PingResponder;
+import demo.Pong;
 import improbable.worker.*;
 
 public class JavaWorker {
 	private static final int FRAMES_PER_SECOND = 60;
 
 	public static void main(String[] args) {
-		if(args.length > 0){
-			for(String arg : args){
-				System.out.println(arg);
-			}
+		if (args.length > 0) {
 			Connection connection = getConnection(args[1], args[0], Integer.parseInt(args[1]));
+			Dispatcher dispatcher = new Dispatcher();
+			registerCallbacks(connection, dispatcher);
+			runEventLoop(connection, dispatcher);
 		} else {
 			System.out.println("No args passed. Exiting...");
 			System.exit(1);
@@ -22,27 +27,40 @@ public class JavaWorker {
 		parameters.workerType = "JavaWorker";
 		parameters.network = new NetworkParameters();
 		parameters.network.connectionType = NetworkConnectionType.Tcp;
-		parameters.network.useExternalIp = false;
+//		parameters.network.useExternalIp = false;
 
 		return Connection.connectAsync(hostname, port, workerId, parameters).get();
 	}
 
+	private static java.util.List<Long> registerCallbacks(Connection connection, Dispatcher dispatcher) {
+		List<Long> callbackKeys = new ArrayList<Long>();
+
+		long callbackKey = dispatcher.onCommandRequest(PingResponder.Commands.PING, request -> {
+			connection.sendLogMessage(LogLevel.INFO, "JavaWorker", "Received GetWorkerType command");
+			Pong pingResponse = new Pong("JavaWorker", "Hello from JavaWorker!");
+			connection.sendCommandResponse(PingResponder.Commands.PING, request.requestId, pingResponse);
+		});
+		callbackKeys.add(callbackKey);
+		
+		return callbackKeys;
+	}
+
 	private static void runEventLoop(Connection connection, Dispatcher dispatcher) {
-		java.time.Duration maxWait = java.time.Duration.ofMillis(Math.round(1000.0 / FRAMES_PER_SECOND));
-		while (true) {
-			long startTime = System.nanoTime();
+//		java.time.Duration maxWait = java.time.Duration.ofMillis(Math.round(1000.0 / FRAMES_PER_SECOND));
+		while (connection.isConnected()) {
+//			long startTime = System.nanoTime();
 			OpList opList = connection.getOpList(0 /* non-blocking */);
 			// Invoke callbacks.
 			dispatcher.process(opList);
 			// Do other work here...
-			long stopTime = System.nanoTime();
-			java.time.Duration waitFor = maxWait.minusNanos(stopTime - startTime);
-			try {
-				Thread.sleep(Math.max(waitFor.toMillis(), 0));
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-				throw new RuntimeException(e);
-			}
+//			long stopTime = System.nanoTime();
+//			java.time.Duration waitFor = maxWait.minusNanos(stopTime - startTime);
+//			try {
+//				Thread.sleep(Math.max(waitFor.toMillis(), 0));
+//			} catch (InterruptedException e) {
+//				Thread.currentThread().interrupt();
+//				throw new RuntimeException(e);
+//			}
 		}
 	}
 
